656036d05babeac8e834b12fd5f601bc1d73abca
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-21 13:32:22 -0500

    adding post page

---
+++ viewpost.php
@@ -0,0 +1,36 @@
+<?php 
+require_once('session.php');
+require_once('database.php');
+	require_once('crud.php'); 
+	Database::connect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('header.php'); ?>
+ 
+<body>
+<?php require_once('nav.php'); ?>
+<br><br><br><br><br><br>
+<h1>story page </h1>
+<?php
+try{
+      
+      $sql = 'SELECT * FROM blog WHERE id = :id';
+      $q = $pdo->prepare($sql);
+      $q->execute(array(':id' => $_GET['id']));
+      $data = $q->fetchAll(PDO::FETCH_ASSOC);
+      echo $data;     
+      return $data;
+      } catch (PDOException $error){
+
+      header( "Location: 500.php" );
+      //echo $error->getMessage();
+    }
+?>
+
+<?php require_once('footer.php');
+ Database::disconnect();
+  ?>
+
+</body>
+</html>
