5943ba4f52572f94073b157493d932008d640882
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-21 07:53:29 -0500

    check jquery version

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
