e8485bb2b12342e2ca2af40e9b9766fc7ac684f3
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-21 12:06:15 -0500

    layout

--- blog.php
+++ blog.php
@@ -88,8 +88,10 @@ Database::disconnect();
 
 	$blog = new blogCrud($_SESSION['uid']);
 	foreach ($blog->read() as $row) { 
+	echo '<table>'	
 	echo '<p>Blog Title:<br>'.$row['blogTitle'].'</p><br>';
-	echo '<p>Blog Post:<br>'.$row['blogPost'].'</p><br><br><br>';	
+	echo '<p>Blog Post:<br>'.$row['blogPost'].'</p><br><br><br>';
+	echo '</table>'	
 	}
 
 ?>
