7f902f9f9172f349654260eb719e45fddb5cc6df
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-21 10:53:16 -0500

    adding styles to blog page

--- blog.php
+++ blog.php
@@ -23,12 +23,13 @@
 <!DOCTYPE html>
 <html>
 
-
+<?php require_once('header.php'); ?>
 
 <body>
 
-
+<?php require_once('nav.php'); ?>
 <br><br><br><br><br><br>
+
 
     <div class="container">
 
@@ -100,7 +101,7 @@
 <br><br><br><br><br><br>
 
 <br><br><br>
-
+<?php require_once('footer.php'); ?>
 
  </body>
 </html>
