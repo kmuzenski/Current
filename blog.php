1415a5f2f940cba1f35d0f802b87790cb55a8d2e
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-23 15:06:57 -0500

    pag test

--- blog.php
+++ blog.php
@@ -109,7 +109,7 @@ Database::disconnect();
 Database::disconnect();?>
 <script>
 $(document).ready(function() {
-var show_per_page = 5;
+var show_per_page = 10;
 var number_of_items = $("#content").children().size();
 var number_of_pages = Math.ceil(number_of_items/show_per_page);
 
