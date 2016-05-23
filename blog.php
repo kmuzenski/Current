e4e7409dbb434f0eefb89144b2ea67bbf8040233
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-23 16:00:06 -0500

    testing search

--- blog.php
+++ blog.php
@@ -110,10 +110,6 @@ Database::disconnect();
 <br><br><br>
 <?php require_once('footer.php'); 
 Database::disconnect();?>
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="typeahead.min.js"></script>
-
 <script>
 $(document).ready(function() {
    $('input.typeahead').typeahead({
--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
+<script src="typeahead.min.js"></script>
 <script src="app.js"></script>
