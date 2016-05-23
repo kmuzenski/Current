eb46fc0f441f5fec0c6234e622b027a128a2c6a5
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-23 16:05:46 -0500

    testing search

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="typeahead.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 <script src="app.js"></script>
