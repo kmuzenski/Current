1a5863e09e74a1936bf0759c3fe9ef938c40fa9f
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 18:22:59 -0500

    testing update

--- updateBlog.php
+++ updateBlog.php
@@ -1,9 +1,10 @@
 <?php
- error_reporting(E_ALL);
+ 
 require_once 'database.php';
 require_once 'session.php';
 require_once 'crud.php';
- 
+error_reporting(E_ALL);
+    
     if ( !empty($_POST)) {
       // keep track post values
       $id = $_POST['id'];
@@ -14,6 +15,13 @@ require_once 'crud.php';
          
       $blog = new blogCrud($_SESSION['uid']);
       $response = $blog->update($blogTitle,$postDate,$blogPost,$id);
+
+      if($response) {
+        header('Location: profile.php');
+      }
+      else {
+        header('Location: update.php')
+      }
 
     }
 
