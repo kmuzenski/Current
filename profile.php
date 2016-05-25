63c21c78f4edab0a2d0786abf09e4fcdba1c4d09
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 17:59:28 -0500

    style change

--- profile.php
+++ profile.php
@@ -56,9 +56,9 @@ require_once('crud.php');
     echo '<form method="POST" action="updateBlog.php">';
     echo '<input type="hidden" name="id" value="'.$row['id'].'">';
     echo '<table class="table table-striped table-bordered">';
-    echo '<tr><td><input type="text" name="blogTitle" value="'.$row['blogTitle'].'"></td></tr>'; 
-    echo '<tr><td><input type="text" name="postDate" value="'.$row['postDate'].'"></td></tr>';
-    echo '<tr><td><input type="text" name="blogPost" value="'.$row['blogPost'].'"></td></tr>';
+    echo '<tr><td><input type="text" name="blogTitle" value="Blog Title: <br>'.$row['blogTitle'].'"></td></tr>'; 
+    echo '<tr><td><input type="text" name="postDate" value="Post Date: <br>'.$row['postDate'].'"></td></tr>';
+    echo '<tr><td><input type="text" name="blogPost" value="Blog Post: <br>'.$row['blogPost'].'"></td></tr>';
     echo '</table>';
     echo '</form>';
    
