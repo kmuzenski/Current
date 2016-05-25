d6ba202003cedd06f4ef06cd80c74544a790b272
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-24 20:38:28 -0500

    testing

--- nytSearch.php
+++ nytSearch.php
@@ -34,7 +34,7 @@ function nyt () {
 		crossDomain: true,
 		//dataType:'jsonp',
 	}).done(function(result) {
-		$.each(result.response.docs, function(key, value){ 
+		$.each(result.docs, function(key, value){ 
 		$("#nyt").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
 
 	
