8b31f80a43871b79784a8f77750abd2748fc08e8
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-24 20:54:14 -0500

    fix

--- nytSearch.php
+++ nytSearch.php
@@ -24,11 +24,11 @@ document.write($_POST["searchTerm"]);
 	$.ajax({
 		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $_POST["searchTerm"] +"&api-key=33b85401cda2437c829b4679e0cd3d35",
 		method: 'POST',
-		crossDomain: true,
+		crossDomain: true
 
 		success: function(result) {
 			
-			$.each(result.response.docs, function(key, value){
+			$.each(result.docs, function(key, value){
 			$("#nyt").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
 				
 		
