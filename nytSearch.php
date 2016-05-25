6347a79d26fcc57077ccdc998a340af977aed315
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-24 20:33:09 -0500

    test

--- nytSearch.php
+++ nytSearch.php
@@ -32,7 +32,7 @@ function nyt () {
 		url: url,
 		method: 'POST',
 		crossDomain: true,
-		dataType:'jsonp',
+		//dataType:'jsonp',
 	}).done(function(result) {
 		$.each(result.response.docs, function(key, value){ 
 		$("#nyt").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
