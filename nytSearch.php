b06819bf05f36f62f725bbf845d9b4e430ceee14
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 20:23:49 -0500

    testing ajax call

--- nytSearch.php
+++ nytSearch.php
@@ -18,7 +18,7 @@
 
 <?php require_once('footer.php'); ?>
 <script>
-function nytSearch() {
+/*function nytSearch() {
 var $_POST = <?php echo json_encode($_POST) ?>;
 document.write($_POST["searchTerm"]);
 
@@ -44,9 +44,37 @@ document.write($_POST["searchTerm"]);
 }
 })	
 }
+*/
 
+
+function nytSearch () {
+	var $_POST = <?php echo json_encode($_POST) ?>;
+	document.write($_POST["searchTerm"]);
+	var url ="https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?=$_POST' + $.param({
+
+	'api-key': "33b85401cda2437c829b4679e0cd3d35"
+	});
+
+	$.ajax({
+		url: url,
+		method: 'GET',
+	}).done(function(result) {
+		$.each(result.response.docs, function(key, value){ 
+		$("#nytSearch").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
+
+	
+		console.log(result);
+	});
+	}).fail(function(err){
+		throw err;
+	});
+}
 
 nytSearch();
+
+
+
 </script>
 
 </body>
