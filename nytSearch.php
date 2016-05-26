3d81e9601581e7219a2d5f80db7a7686d14393de
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 21:03:08 -0500

    removed testing code

--- nytSearch.php
+++ nytSearch.php
@@ -20,7 +20,7 @@
 <script>
 function nytSearch() {
 var $_POST = <?php echo json_encode($_POST) ?>;
-document.write($_POST["searchTerm"]);
+=
 
  return	$.ajax({
 		method:'GET',
@@ -31,13 +31,13 @@ document.write($_POST["searchTerm"]);
 		
 
 		success: function(result) {
-			$("#nytSearch").append('<p>TESTING</p>');
+		
 			
 			$.each(result.response.docs, function(key, value){
 				console.log(result);
 
 			$("#nytSearch").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
-			$("#nytSearch").append('<p>TESTING</p>');	
+				
 		
 			});
 			
@@ -47,33 +47,6 @@ document.write($_POST["searchTerm"]);
 }
 
 
-/*
-function nytSearch () {
-	var $_POST = <?php echo json_encode($_POST) ?>;
-	document.write($_POST["searchTerm"]);
-	var url ="https://api.nytimes.com/svc/search/v2/articlesearch.json" + "?=$_POST";
-	url += '?=$_POST' +  $.param({
-
-	'api-key': "33b85401cda2437c829b4679e0cd3d35"
-	});
-
-	$.ajax({
-		url: url,
-		method: 'POST',
-		//dataType:'jsonp',
-		//crossDomain: true,
-	}).done(function(result) {
-		$.each(result.response.docs, function(key, value){ 
-		$("#nytSearch").append('<table class="table table-striped table-bordered"><tr><td><h4><strong>' + value.headline.main + '</strong></h4></td></tr><tr><td>'  + value.snippet + '</td></tr><tr><td><a href="' + value.web_url + '"target="_blank">View Article</a></td></tr></table>');
-
-	
-		console.log(result);
-	});
-	}).fail(function(err){
-		throw err;
-	});
-}
-*/
 nytSearch();
 
 
