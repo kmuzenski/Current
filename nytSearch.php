98c2f0218fbd4807d603d792712e4822c03743d7
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 20:40:20 -0500

    test

--- nytSearch.php
+++ nytSearch.php
@@ -18,16 +18,16 @@
 
 <?php require_once('footer.php'); ?>
 <script>
-/*function nytSearch() {
-var $_POST = <?php echo json_encode($_POST) ?>;
-document.write($_POST["searchTerm"]);
+function nytSearch() {
+//var $_POST = <?php echo json_encode($_POST) ?>;
+//document.write($_POST["searchTerm"]);
 
  return	$.ajax({
 		method:'GET',
 		dataType:'json',
 		
 		//crossDomain: true,
-		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=globalwarming&api-key=33b85401cda2437c829b4679e0cd3d35",
+		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?query=globalwarming&api-key=33b85401cda2437c829b4679e0cd3d35",
 		
 
 		success: function(result) {
@@ -44,14 +44,14 @@ document.write($_POST["searchTerm"]);
 }
 })	
 }
-*/
 
 
+/*
 function nytSearch () {
 	var $_POST = <?php echo json_encode($_POST) ?>;
 	document.write($_POST["searchTerm"]);
 	var url ="https://api.nytimes.com/svc/search/v2/articlesearch.json" + "?=$_POST";
-	url += /*'?=$_POST' + */ $.param({
+	url += '?=$_POST' +  $.param({
 
 	'api-key': "33b85401cda2437c829b4679e0cd3d35"
 	});
@@ -72,7 +72,7 @@ function nytSearch () {
 		throw err;
 	});
 }
-
+*/
 nytSearch();
 
 
