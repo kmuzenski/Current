d588e09a9a2c99419a9e71ff112d95ef0b67e54c
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-25 20:17:50 -0500

    test

--- nytSearch.php
+++ nytSearch.php
@@ -24,7 +24,8 @@ document.write($_POST["searchTerm"]);
 
  return	$.ajax({
 		method:'GET',
-		//dataType:'jsonp',
+		dataType:'jsonp',
+		cache: false,
 		//crossDomain: true,
 		url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=globalwarming&api-key=33b85401cda2437c829b4679e0cd3d35",
 		
