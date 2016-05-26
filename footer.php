e8f29d7b14194c9bfe9e8142eca532d3b3f5c5ed
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-05-26 14:59:27 -0500

    footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="app.js"></script>
-<a href="sitemap.php" alt="sitemap">Site Map</a>
+<p><small>&copy;Current&nbsp;&nbsp;<a href="sitemap.php" alt="sitemap" title="sitemap">Site Map</a></small></p>
--- index.php
+++ index.php
@@ -42,6 +42,7 @@
 
 </div>
 </div>
+<br><br><br><br><br><br>
 <?php require_once('footer.php'); ?>
 
 </body>
