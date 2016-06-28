a7d375788105f6c91e5f6e29404c55d7b87bcc11
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-28 13:05:56 -0500

    layout

--- index.php
+++ index.php
@@ -22,15 +22,15 @@
 <div class="container">
 <div class="row">
 
-<div class="col-xs-2">
+<div class="col-xs-3">
 </div>
 
-<div class="col-xs-6">
+<div class="col-xs-4">
 <div id="result">
 </div>
 </div>
 
-<div class="col-xs-2">
+<div class="col-xs-3">
 </div>
 
 </div>
