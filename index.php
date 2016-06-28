76ff3f229e463a76b241f5e5040b609dd5ef1300
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-28 12:57:08 -0500

    style change

--- index.php
+++ index.php
@@ -22,15 +22,15 @@
 <div class="container">
 <div class="row">
 
-<div class="col-xs-4">
+<div class="col-xs-2">
 </div>
 
-<div class="col-xs-4">
+<div class="col-xs-6">
 <div id="result">
 </div>
 </div>
 
-<div class="col-xs-4">
+<div class="col-xs-2">
 </div>
 
 </div>
