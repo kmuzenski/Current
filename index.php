93c27a54bbcc3ca13a702f5fc8d77496a79d632a
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-27 22:42:38 -0500

    adding images

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
 <body>
 <?php require_once('nav.php'); ?>
 <br><br><br><br><br><br>
-
+<div class="container">
+<div class="jumbotron">
 <h1>Current<img src="assets/images/wave.png" alt="logo" title="logo" width="100px"></h1>
 <br>
 <p>Search for the news stories you want to see</p>
 <br>
 <input type="text" id="search" placeholder="search terms">
 <input type="submit" value="search" id="getnews">
+</div>
+</div>
 <br><br><br><br><br><br>
 
 
